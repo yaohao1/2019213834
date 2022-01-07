491e12a660f986775cea8e379dc1d44e16f45f64
yaohao1 <86462545+yaohao1@users.noreply.github.com>
2022-01-07 13:40:15 +0800

    Add files via upload

---
+++ 02/js/information.js
@@ -0,0 +1,220 @@
+ function move1(event)
+ {
+       var ei = document.getElementById("enlarge_images1");
+       event = event || window.event;
+		ei.style.display = "block";
+		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out1(event)
+ {
+       var ei = document.getElementById("enlarge_images1");
+        ei.innerHTML = "";
+		ei.style.display = "none";
+ }
+ 
+ function move2(event)
+ {
+       var ei = document.getElementById("enlarge_images2");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out2(event)
+ {
+       var ei = document.getElementById("enlarge_images2");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move3(event)
+ {
+       var ei = document.getElementById("enlarge_images3");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out3(event)
+ {
+       var ei = document.getElementById("enlarge_images3");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move4(event)
+ {
+       var ei = document.getElementById("enlarge_images4");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out4(event)
+ {
+       var ei = document.getElementById("enlarge_images4");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move5(event)
+ {
+       var ei = document.getElementById("enlarge_images5");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out5(event)
+ {
+       var ei = document.getElementById("enlarge_images5");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move6(event)
+ {
+       var ei = document.getElementById("enlarge_images6");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out6(event)
+ {
+       var ei = document.getElementById("enlarge_images6");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move7(event)
+ {
+       var ei = document.getElementById("enlarge_images7");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out7(event)
+ {
+       var ei = document.getElementById("enlarge_images7");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move8(event)
+ {
+       var ei = document.getElementById("enlarge_images8");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out8(event)
+ {
+       var ei = document.getElementById("enlarge_images8");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move9(event)
+ {
+       var ei = document.getElementById("enlarge_images9");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out9(event)
+ {
+       var ei = document.getElementById("enlarge_images9");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move10(event)
+ {
+       var ei = document.getElementById("enlarge_images10");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out10(event)
+ {
+       var ei = document.getElementById("enlarge_images10");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move11(event)
+ {
+       var ei = document.getElementById("enlarge_images11");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out11(event)
+ {
+       var ei = document.getElementById("enlarge_images11");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move12(event)
+ {
+       var ei = document.getElementById("enlarge_images12");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out12(event)
+ {
+       var ei = document.getElementById("enlarge_images12");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
+ 
+ function move13(event)
+ {
+       var ei = document.getElementById("enlarge_images13");
+       event = event || window.event;
+ 		ei.style.display = "block";
+ 		ei.innerHTML = '<img src="' + event.srcElement.src + '" />'+event.srcElement.title;
+ 		ei.style.top  = document.body.scrollTop + event.clientY + 10 + "px";
+ 		ei.style.left = document.body.scrollLeft + event.clientX + 10 + "px";
+ }
+ 
+ function out13(event)
+ {
+       var ei = document.getElementById("enlarge_images13");
+        ei.innerHTML = "";
+ 		ei.style.display = "none";
+ }
